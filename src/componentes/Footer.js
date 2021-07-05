56473865502d0ce3552039d0804ddf64a00deb44
danilo-mart <daniloms@msn.com>
2021-07-05 20:24:09 -0300

    itens

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,11 +1,11 @@
-import React from "react";
-import adicionar from '../assets/icones/adicionar.svg'
+import React from 'react';
+import adicionar from '../assets/icones/adicionar.svg';
 
 export const Footer = () => {
     return (
         <div className="container-footer">
-        <button><img src={adicionar} alt="Adicionar tarefa"/>Adicionar tarefa </button>
-        <span>© Copyright {new Date().getFullYear()} Devaria. Todos os direitos reservados.</span>
+            <button><img src={adicionar} alt="Adicionar tarefa" />Adicionar tarefa</button>
+            <span>© Copyright {new Date().getFullYear()} Devaria. Todos os direitos reservados.</span>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,38 @@
-import React from "react";
-import {Header} from '../componentes/Header';
-import {Filtros} from '../componentes/Filtros';
-import { Listagem } from "../componentes/Listagem";
-import { Footer } from "../componentes/Footer";
+import React, { useState } from 'react'
+import { Header } from '../componentes/Header';
+import { Filtros } from '../componentes/Filtros';
+import { Listagem } from '../componentes/Listagem';
+import { Footer } from '../componentes/Footer';
 
 export const Home = props => {
 
+    const [tarefas, setTarefas] = useState([
+        {
+            id: '123fczasd',
+            nome: 'Tarefa Mock 1',
+            dataPrevisaoConclusao: '2021-07-03',
+            dataConclusao: null
+        },
+        {
+            id: 'rfasf123123',
+            nome: 'Tarefa Mock 2',
+            dataPrevisaoConclusao: '2021-07-01',
+            dataConclusao: '2021-06-30'
+        }
+    ]);
+
     const sair = () => {
         localStorage.removeItem('accessToken');
-            localStorage.removeItem('usuarioNome');
-            localStorage.removeItem('usuarioEmail');   
-            props.setAccessToken(''); 
+        localStorage.removeItem('usuarioNome');
+        localStorage.removeItem('usuarioEmail');
+        props.setAccessToken('');
     }
-    return(
+
+    return (
         <>
             <Header sair={sair}/>
             <Filtros />
-            <Listagem />
+            <Listagem tarefas={tarefas}/>
             <Footer />
         </>
     );
